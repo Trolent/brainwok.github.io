a2cc47a26bbf5a00fe706cc27b674b0965de2928
BrainWok <78040062+BrainWok@users.noreply.github.com>
2021-01-27 17:24:18 +0100

    Update tools.js

--- tools.js
+++ tools.js
@@ -11,43 +11,43 @@ var width = 16384;
 var height = 16384;
 
 let casesCouleur = [
-//A trouver pour l'énigme
- //1re recherche
-  ["m48_303837","4.072ap406","0,255,0","Kyan"],
- //2e recherche
-  ["0b-ennb#5_","bp#_bt!oes","0,255,0","Elephant #1"],
-  ["!r@n#4_een","_e#_0lsr$x","0,255,0","Elephant #2"],
-  ["22$v#f_-01","vso#_fub-i","0,255,0","Elephant #3"],
-//Refs UBM
-  ["unebonnebo","utique_com","255,0,0","Mug UBM"],
-  ["x0-#o98eyl","_e#_0lsr$x","255,0,0","Navo"],
-  ["cp4luiki-f","0cherch@_i","255,0,0","Charlie"],
-  ["$pa.dut0o_","la@_dzol3y","255,0,0","Laura Felpin"],
-  ["44uv-n3!b@","?44n0ns@!m","255,0,0","Penelope Bagieu"],
-//Incrustations
- // Objets
-  ["7g8nolkfs$","#3$r0m-zat","255,0,0","Borne Incendie"],
-  ["88j7#b5vwm","28#2-8vql0","255,0,0","Noix"],
-  ["88j7#b5vwm","r$tk.t7p2o","255,0,0","Chapeau Shérif"],
- // Déguisements
-  ["17a16_!29#","#h##.5?v#-","255,0,0","Cône"],
-  ["#$5#lj_#f_","czo$l.#3s$","255,0,0","Lampe de Génie"],
-  ["zrpj4x.vh$","sq_9v8ol$#","255,0,0","Bière Corona"],
- // Cinéma
-  ["iw_.w--gl3","dggwitbkak","255,0,0","Stormtrooper"],
-  ["88j7#b5vwm","c8bk#g1v@-","255,0,0","RoboCop"],
- //Animaux
-  ["ou.qp#j?q-","mj@7bgjdvy","255,0,0","Chat"],
- //Objets sur la tête
-  ["$ltd_4l#2z","c_wfg#4yj3","255,0,0","Sceau sur la tête"],
- //Autre
-  [".mkz4_9_r$","@z3#_531u!","255,0,0","Pizza"],
-  ["7g8nolkfs$","!?#whpqjzr","255,0,0","Homme qui tombe"],
-  ["bvy2#xk!f#","0vry5i5e8b","255,0,0","Chaise roulante"],
-  ["ge-9ep9##u","8_$2tl2d1k","255,0,0","Pelleteuse"], 
-  ["cxus5zni5o","j_h335xqk.","255,0,0","Loupe"],
-  ["@l#gy33##e","664--wcane","255,0,0","Oignon"],
-  ["-7g4pvs80.","ahi3yh-lo_","255,0,0","Briques"]
+  //A trouver pour l'énigme
+   //1re recherche
+    ["m48_303837","4.072ap406","0,255,0","Kyan"],
+   //2e recherche
+    ["0b-ennb#5_","bp#_bt!oes","0,255,0","Elephant #1"],
+    ["!r@n#4_een","_e#_0lsr$x","0,255,0","Elephant #2"],
+    ["22$v#f_-01","vso#_fub-i","0,255,0","Elephant #3"],
+  //Refs UBM
+    ["unebonnebo","utique_com","255,0,0","Mug UBM"],
+    ["x0-#o98eyl","_e#_0lsr$x","255,0,0","Navo"],
+    ["cp4luiki-f","0cherch@_i","255,0,0","Charlie"],
+    ["$pa.dut0o_","la@_dzol3y","255,0,0","Laura Felpin"],
+    ["44uv-n3!b@","?44n0ns@!m","255,0,0","Penelope Bagieu"],
+  //Incrustations
+   // Objets
+    ["7g8nolkfs$","#3$r0m-zat","255,0,0","Borne Incendie"],
+    ["88j7#b5vwm","28#2-8vql0","255,0,0","Noix"],
+    ["88j7#b5vwm","r$tk.t7p2o","255,0,0","Chapeau Shérif"],
+   // Déguisements
+    ["17a16_!29#","#h##.5?v#-","255,0,0","Cône"],
+    ["#$5#lj_#f_","czo$l.#3s$","255,0,0","Lampe de Génie"],
+    ["zrpj4x.vh$","sq_9v8ol$#","255,0,0","Bière Corona"],
+   // Cinéma
+    ["iw_.w--gl3","dggwitbkak","255,0,0","Stormtrooper"],
+    ["88j7#b5vwm","c8bk#g1v@-","255,0,0","RoboCop"],
+   //Animaux
+    ["ou.qp#j?q-","mj@7bgjdvy","255,0,0","Chat"],
+   //Objets sur la tête
+    ["$ltd_4l#2z","c_wfg#4yj3","255,0,0","Sceau sur la tête"],
+   //Autre
+    [".mkz4_9_r$","@z3#_531u!","255,0,0","Pizza"],
+    ["7g8nolkfs$","!?#whpqjzr","255,0,0","Homme qui tombe"],
+    ["bvy2#xk!f#","0vry5i5e8b","255,0,0","Chaise roulante"],
+    ["ge-9ep9##u","8_$2tl2d1k","255,0,0","Pelleteuse"],
+    ["cxus5zni5o","j_h335xqk.","255,0,0","Loupe"],
+    ["@l#gy33##e","664--wcane","255,0,0","Oignon"],
+    ["-7g4pvs80.","ahi3yh-lo_","255,0,0","Briques"]
 ];
 
 function stopWheelZoom(event) {
@@ -175,7 +175,7 @@ function makeGrid(gSize) {
 function loadMenu() {
   let menu = document.getElementById("obj_menu");
   for (i = 0; i < casesCouleur.length; i++) {
-    menu.insertAdjacentHTML( 'beforeend', "<li><a href=\"#\" onclick=\"scrollToXY(\'"+casesCouleur[i][0]+"\',\'"+casesCouleur[i][1]+"\');return false;\">"+casesCouleur[i][3]+"</a></li>" );       
+    menu.insertAdjacentHTML( 'beforeend', "<li><a href=\"#\" onclick=\"scrollToXY(\'"+casesCouleur[i][0]+"\',\'"+casesCouleur[i][1]+"\');return false;\">"+casesCouleur[i][3]+"</a></li>" );
   }
 }
 
